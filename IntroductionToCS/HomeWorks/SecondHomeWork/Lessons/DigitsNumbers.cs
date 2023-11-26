8d4bf70a9766530d78b6037b668b7025617e9b5e
name <test@mail.com>
2023-11-26 13:51:22 +0300

    Цифры числа

--- IntroductionToCS/HomeWorks/SecondHomeWork/Lessons/DigitsNumbers.cs
+++ IntroductionToCS/HomeWorks/SecondHomeWork/Lessons/DigitsNumbers.cs
@@ -9,13 +9,53 @@ namespace IntroductionToCS.HomeWorks.SecondHomeWork.Lessons
 {
     class DigitsNumbers : ILesson
     {
-        public bool CanRun => false;
+        public bool CanRun => true;
 
         public string Title => "Цифры числа";
 
         public void Run()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Введите число:");
+            var data = Console.ReadLine();
+
+            if(!int.TryParse(data, out int number))
+            {
+                Console.WriteLine("Не удалось получить число.");
+                return;
+            }
+
+            Console.Write($"{number} => ");
+
+            if(number < 10)
+            {
+                Console.WriteLine($"{number}");
+                return;
+            }
+
+            int invertedNumber = 0;
+
+            while(number > 0)
+            {
+                int digit = number % 10;
+                number /= 10;
+                invertedNumber = invertedNumber * 10 + digit;
+            }
+
+            bool isFirstDigit = true;
+
+            while(invertedNumber > 0)
+            {
+                if(!isFirstDigit)
+                    Console.Write($", ");
+
+                int digit = invertedNumber % 10;
+                invertedNumber /= 10;
+
+                Console.Write($"{digit}");
+                isFirstDigit = false;
+            }
+
+            Console.WriteLine();
         }
     }
 }
