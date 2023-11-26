a2fa6f865b85d842c64597baa568ab478c018dca
name <test@mail.com>
2023-11-26 13:06:02 +0300

    Кратность числа на 7 и 23

--- IntroductionToCS/HomeWorks/SecondHomeWork/Lessons/MultiplicityOfTheNumber.cs
+++ IntroductionToCS/HomeWorks/SecondHomeWork/Lessons/MultiplicityOfTheNumber.cs
@@ -9,13 +9,26 @@ namespace IntroductionToCS.HomeWorks.SecondHomeWork.Lessons
 {
     class MultiplicityOfTheNumber : ILesson
     {
-        public bool CanRun => false;
+        public bool CanRun => true;
 
         public string Title => "Кратность числа на 7 и 23";
 
         public void Run()
         {
+            Console.WriteLine("Введите число:");
+            string userData = Console.ReadLine();
 
+            if(!int.TryParse(userData, out int number))
+            {
+                Console.WriteLine("Не удалось получить число.");
+                return;
+            }
+
+            Console.Write($"Число {number} => ");
+            if(number % 7 == 0 && number % 23 == 0)
+                Console.WriteLine("да.");
+            else
+                Console.WriteLine("нет.");
         }
     }
 }
