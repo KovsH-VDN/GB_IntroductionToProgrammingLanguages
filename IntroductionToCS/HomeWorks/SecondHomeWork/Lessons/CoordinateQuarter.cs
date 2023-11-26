4d9828cec2a9d6bcb9735855a3e1b88bd8db0f6a
name <test@mail.com>
2023-11-26 13:24:22 +0300

    Координатная четверть

--- IntroductionToCS/HomeWorks/SecondHomeWork/Lessons/CoordinateQuarter.cs
+++ IntroductionToCS/HomeWorks/SecondHomeWork/Lessons/CoordinateQuarter.cs
@@ -9,13 +9,44 @@ namespace IntroductionToCS.HomeWorks.SecondHomeWork.Lessons
 {
     class CoordinateQuarter : ILesson
     {
-        public bool CanRun => false;
+        public bool CanRun => true;
 
         public string Title => "Координатная четверть";
 
         public void Run()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Введите координату X:");
+            string userData = Console.ReadLine();
+
+            if(!int.TryParse(userData, out int xCoordinate))
+            {
+                Console.WriteLine("Не удалось получить координату X.");
+                return;
+            }
+            Console.WriteLine("Введите координату Y:");
+            userData = Console.ReadLine();
+
+            if(!int.TryParse(userData, out int yCoordinate))
+            {
+                Console.WriteLine("Не удалось получить координату Y.");
+                return;
+            }
+
+            if(xCoordinate == 0 || yCoordinate == 0)
+            {
+                Console.WriteLine("Одна из координа равно 0");
+                return;
+            }
+
+            Console.Write($"{xCoordinate}, {yCoordinate} => ");
+            if(xCoordinate > 0 && yCoordinate > 0)
+                Console.WriteLine("1");
+            else if(xCoordinate < 0 && yCoordinate < 0)
+                Console.WriteLine("3");
+            else if(xCoordinate > 0 && yCoordinate < 0)
+                Console.WriteLine("4");
+            else
+                Console.WriteLine("2");
         }
     }
 }
