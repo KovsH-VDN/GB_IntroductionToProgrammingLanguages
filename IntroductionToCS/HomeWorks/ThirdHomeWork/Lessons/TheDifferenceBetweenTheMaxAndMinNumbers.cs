256c0ab97718eca18fdffd9fa516f19035095cf0
Kovsh <kovsh@mail.com>
2023-11-27 16:34:29 +0300

    дз 3 задача 3 - Разница между максимальным и минимальным числами в массиве

--- IntroductionToCS/HomeWorks/ThirdHomeWork/Lessons/TheDifferenceBetweenTheMaxAndMinNumbers.cs
+++ IntroductionToCS/HomeWorks/ThirdHomeWork/Lessons/TheDifferenceBetweenTheMaxAndMinNumbers.cs
@@ -9,13 +9,63 @@ namespace IntroductionToCS.HomeWorks.ThirdHomeWork.Lessons
 {
     class TheDifferenceBetweenTheMaxAndMinNumbers : ILesson
     {
-        public bool CanRun => false;
+        public bool CanRun => true;
 
         public string Title => "Разница между максимальным и минимальным числами в массиве";
 
         public void Run()
         {
-            throw new NotImplementedException();
+            int minNumber = 1;
+            int maxNumber = 100;
+
+            int arraySize = 10;
+
+            double[] arrayOfnumbers = new double[arraySize];
+            Random random = new Random();
+
+            double min = double.MaxValue;
+            double max = double.MinValue;
+
+            for(int i = 0; i < arraySize; ++i)
+            {
+                double fractionalPart = 0;
+                while(fractionalPart == 0)
+                {
+                    fractionalPart = random.NextDouble();
+                }
+
+                double currentnumber = Math.Round(random.Next(minNumber, maxNumber) + fractionalPart, 2);
+                arrayOfnumbers[i] = currentnumber;
+
+                if(min > currentnumber)
+                    min = currentnumber;
+
+                if(max < currentnumber)
+                    max = currentnumber;
+            }
+
+            Console.Write("Разница между максимальным и минимальным элементами\nв массиве [");
+
+            for(int i = 0; i < arraySize; ++i)
+            {
+                if(i > 0)
+                    Console.Write($", ");
+
+                double currentNumber = arrayOfnumbers[i];
+                if(currentNumber == min)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else if(currentNumber == max)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+
+                Console.Write($"{currentNumber}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine($"] => {max - min}");
         }
     }
 }
