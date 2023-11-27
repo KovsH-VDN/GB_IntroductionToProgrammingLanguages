5ec0edbdd662b9f5c589b16d2cbed4ebd32e37bd
Kovsh <kovsh@mail.com>
2023-11-27 16:13:30 +0300

    дз 3 задача 2 - Четные числа

--- IntroductionToCS/HomeWorks/ThirdHomeWork/Lessons/EvenNumbers.cs
+++ IntroductionToCS/HomeWorks/ThirdHomeWork/Lessons/EvenNumbers.cs
@@ -9,13 +9,49 @@ namespace IntroductionToCS.HomeWorks.ThirdHomeWork.Lessons
 {
     class EvenNumbers : ILesson
     {
-        public bool CanRun => false;
+        public bool CanRun => true;
 
         public string Title => "Четные числа";
 
         public void Run()
         {
-            throw new NotImplementedException();
+            int minNumber = 1;
+            int maxNumber = 100;
+
+            int arraySize = 10;
+
+            int[] arrayOfnumbers = new int[arraySize];
+            Random random = new Random();
+
+            for(int i = 0; i < arraySize; ++i)
+            {
+                arrayOfnumbers[i] = random.Next(minNumber, maxNumber);
+            }
+
+            Console.Write("В массиве [");
+
+            int countNumberFromRange = 0;
+            for(int i = 0; i < arraySize; ++i)
+            {
+                if(i > 0)
+                    Console.Write($", ");
+
+                int currentNumber = arrayOfnumbers[i];
+                if(currentNumber % 2 == 0)
+                {
+                    ++countNumberFromRange;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+
+                Console.Write($"{currentNumber}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine($"] {countNumberFromRange} четных чисел");
         }
     }
 }
