073d3a2396a37b9bfbff01f53a9837459dd87382
Kovsh <kovsh@mail.com>
2023-11-27 20:45:30 +0300

    Добавлен помощьник. для получения данных от пользователя из консоли

---
+++ IntroductionToCS/Common/ConsoleHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToCS.Common
+{
+    public static class ConsoleHelper
+    {
+        public static string GetString(string title)
+        {
+            Console.WriteLine($"Введите {title}");
+            return Console.ReadLine();
+        }
+        public static int GetInt32()
+        {
+            var data = GetString("целое число");
+            if(!int.TryParse(data, out int number))
+                return number;
+
+            return -1;
+        }
+        public static long GetInt64()
+        {
+            var data = GetString("целое число");
+            if(!long.TryParse(data, out long number))
+                return number;
+
+            return -1;
+        }
+        public static double GetDouble()
+        {
+            var data = GetString("натуральное число");
+            if(!double.TryParse(data, out double number))
+                return number;
+
+            return double.NaN;
+        }
+    }
+}
