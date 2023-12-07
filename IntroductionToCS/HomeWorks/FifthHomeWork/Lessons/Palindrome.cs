140eb35a6015dbe43045d3536a2af7bf2b8a2efa
Kovsh <kovsh@mail.com>
2023-12-07 19:43:23 +0300

    ДЗ 5, задание 3

--- IntroductionToCS/HomeWorks/FifthHomeWork/Lessons/Palindrome.cs
+++ IntroductionToCS/HomeWorks/FifthHomeWork/Lessons/Palindrome.cs
@@ -9,12 +9,20 @@ namespace IntroductionToCS.HomeWorks.FifthHomeWork.Lessons
 {
     class Palindrome : ILesson
     {
-        public bool CanRun => false;
+        public bool CanRun => true;
 
         public string Title => "Палиндром";
 
         public void Run()
         {
+            var str = Console.ReadLine();
+
+            int take = str.Length / 2;
+            int skip = take + (str.Length % 2 == 0 ? 0 : 1);
+            var leftStr = new string(str.Take(take).ToArray());
+            var rightStr = new string(str.Skip(skip).Take(take).Reverse().ToArray());
+
+            Console.WriteLine($"Слово \"{str}\" {(leftStr == rightStr ? "" : "не ")}является палиндромом");
 
         }
     }
